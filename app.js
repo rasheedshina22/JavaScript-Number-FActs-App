db31b8b4d13f2ffee199cb08ef129889457b8d13
shina <rasheedshinaopeyemi@gmail.com>
2019-05-03 11:00:45 +0100

    my second commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const fact = document.querySelector('#fact');
+const factText = document.querySelector('#factText');
+const numberInput = document.querySelector('#numberInput');
+numberInput.addEventListener('input', fetchFact)
+
+
+function fetchFact() {
+    const number = numberInput.value;
+    fetch(`http://numbersapi.com/${number}`)
+        .then(function (res) {
+            return res.text();
+        })
+        .then(function (data) {
+            if (number !== '') {
+                fact.style.display = 'block';
+                factText.innerHTML = data;
+            } else {
+                fact.style.display = 'none';
+            }
+        })
+        .catch(function () { 
+            console.log(err);
+        });
+    
+}
